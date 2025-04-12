9db88f18461102425979a6438c76e148d0b86d5b
BMB <tzbmb4713@gmail.com>
2025-04-12 13:39:56 +0300

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -253,7 +253,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send audio
         await conn.sendMessage(from, {
-            audio: { url: 'https://github.com/bmb300/NOVA-XMD-DATA/raw/refs/heads/main/KingHans/Menu.mp3' },
+            audio: { url: 'https://files.catbox.moe/wify3q.mp3' },
             mimetype: 'audio/mp4',
             ptt: true
         }, { quoted: mek });
