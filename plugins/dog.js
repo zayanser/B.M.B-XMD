93f8b8c9809fdd91d8e87ccbf9f65dea9b6c0727
BMB <tzbmb4713@gmail.com>
2025-04-13 16:22:39 +0300

    Update dog.js

--- plugins/dog.js
+++ plugins/dog.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.message }, caption: '> *© 𝗡𝗢𝗩𝗔-𝗫𝗠𝗗  ' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.message }, caption: '> *©𝐁.𝐌.𝐁-𝐗𝐌𝐃  ' }, { quoted: mek });
     } catch (e) {
         console.log(e); // ❯❯ Vᴏʀᴛᴇx xᴍᴅ 
         reply(`єяяσя ƒєт¢нιηg ∂σg ιмαgє: ${e.message}`);
