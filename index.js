dbc755ee03ccc0791751ff673d976a64705c1729
B.M.B-XMD <tzbmb4713@gmail.com>
2025-04-14 03:50:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ const {
   //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/sessions/creds.json')) {
 if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
-const sessdata = config.SESSION_ID.replace("Caseyrhodes~", '');
+const sessdata = config.SESSION_ID.replace("bmbxmd~", '');
 const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
 if(err) throw err
